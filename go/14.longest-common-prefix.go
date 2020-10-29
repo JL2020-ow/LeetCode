fb3a1a71b16fbf1b7fb259efaffc2f6776670fbc
JavaLearning2020 <williamalex22@126.com>
2020-10-29 20:44:42 +0800

    first commit

---
+++ go/14.longest-common-prefix.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=14 lang=golang
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+func longestCommonPrefix(strs []string) string {
+
+}
+
+// @lc code=end
+
---
+++ go/7.reverse-integer.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=7 lang=golang
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+func reverse(x int) int {
+
+}
+
+// @lc code=end
+
